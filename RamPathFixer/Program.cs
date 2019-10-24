6be9f3199d63f7ecb0855caf84f0b5dc2ba10c09
Bram Sommen <bram@t-move.be>
2019-10-24 09:35:10 +0200

    Aanpassingen tbv weergave opmaak settingsfile bij opstart programma

--- RamPathFixer/Program.cs
+++ RamPathFixer/Program.cs
@@ -21,6 +21,27 @@ namespace RamPathFixer
 
         private static void Run()
         {
+            // Veerbeeld weergeven van settingsfile
+            // Veerbeeld weergeven van settingsfile
+            // Veerbeeld weergeven van settingsfile
+            Console.WriteLine("C:\\RamPathFixer\\Settings.txt - wordt ingelezen");
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+            Console.WriteLine();
+
+            Console.WriteLine($"D:\\control\\m60-c1\\data\\cnc\\prg;Pad waar de CNC programma's worden geplaatst door Homag optimalisator");
+            Console.WriteLine($"D:\\control\\m60-c1\\data\\cnc\\prg\\OrgHomagOpti; Pad waar de Homag programmas als origineel naartoe worden gecopierd");
+            Console.WriteLine($"D:\\control\\m60-c1\\data\\cnc\\prg\\NewAfterRamPathFixer; Pad waar de bewerkte programma's worden ebxaard (alvorens deze terug naar de root te kopieren");
+
+
+            Console.WriteLine();
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+            Console.WriteLine("VOORBEELD SETTINGS FILE /////////////////////////////");
+
+            Console.WriteLine();
+
             try
             {
                 using (StreamReader reader = new StreamReader("C:\\RamPathFixer\\Settings.txt"))
@@ -52,6 +73,8 @@ namespace RamPathFixer
                     ArchiveLog.NewAfterRamPathFixerPath = NewAfterRamPathFixerPath[0];
                     // TEST PATH
                     xxx = (new DirectoryInfo(ArchiveLog.NewAfterRamPathFixerPath)).GetFiles().ToList();
+
+                    Console.WriteLine("Settingsfile succesvol ingelezen");
                 }
 
                 // Als het bestand correct is uitgelezen
